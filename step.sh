b8be26de4417657be375017bd81d7771da6fa8dd
arouissi <arouissi.externe@pagesjaunes.fr>
2017-07-26 16:36:08 +0200

    Update step.sh

--- step.sh
+++ step.sh
@@ -7,7 +7,6 @@ set -x
 #extraire le numero de jira depuis le message du commit
 #commit_msg="$CHANGELOG"
 commit_msg="cn [#TM-163] klk;n d [#TM-164]jkhdk[#TM-162]j"
-#commit_msg="cnqdfqdfqfqsfqs fqsdfqsfqsfj"
 
 numero_jira="$(echo "$commit_msg" | grep -o '\[#[a-zA-Z.0-9.-]*\]' | sed 's/\[#//g' | sed 's/\]//g')"
 #supprimer les espaces
@@ -18,7 +17,7 @@ numero_jira="$(echo "$numero_jira" | xargs -n1 | sort -u | xargs)"
 
 
 for jira in $numero_jira; do
-curl -D- -u $LOGIN_JIRA:$PASS_JIRA -X POST --data "{\"body\":\"Le correctif de cette jira est passé sur la branche $BITRISE_GIT_BRANCH \n La version $PJ_BUILD_VERSION $PJ_BUILD_NUMBER est disponible sur Beta .\"}" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/$jira/comment -v
+curl -D- -u $LOGIN_JIRA:$PASS_JIRA -X POST --data "{\"body\":\"Le correctif de cette jira est passé sur la branche $BITRISE_GIT_BRANCH \n La version $PJ_BUILD_VERSION $PJ_BUILD_NUMBER est disponible sur Beta .\n[Voir le détail du build|$BITRISE_BUILD_URL]\"}" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/$jira/comment -v
 #changer le status en correction
 curl -D- -u $LOGIN_JIRA:$PASS_JIRA -X POST  --data "{\"transition\":{\"id\":\"761\"}}" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/$jira/transitions?expand=transitions.fields -v
 #changer le status en livraison
