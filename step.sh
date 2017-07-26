c393e2dd762c9e0d793a748efe9071a4a4907492
arouissi <arouissi.externe@pagesjaunes.fr>
2017-07-26 17:13:56 +0200

    Update step.sh

--- step.sh
+++ step.sh
@@ -16,7 +16,7 @@ numero_jira="$(echo "$numero_jira" | xargs -n1 | sort -u | xargs)"
 
 
 for jira in $numero_jira; do
-curl -D- -u $LOGIN_JIRA:$PASS_JIRA -X POST --data "{\"body\":\"Le correctif de cette jira est passé sur la branche $BITRISE_GIT_BRANCH \n La version $PJ_BUILD_VERSION $PJ_BUILD_NUMBER est disponible sur Beta .\n[Voir le détail du build|$BITRISE_BUILD_URL]\"}" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/$jira/comment -v
+curl -D- -u $LOGIN_JIRA:$PASS_JIRA -X POST --data "{\"body\":\"Le correctif de cette jira est passé sur la branche $BITRISE_GIT_BRANCH \n La version $PJ_BUILD_VERSION ($BUILD_VERSIONCODE) est disponible sur Beta .\n[Voir le détail du build|$BITRISE_BUILD_URL]\"}" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/$jira/comment -v
 #changer le status en correction
 curl -D- -u $LOGIN_JIRA:$PASS_JIRA -X POST  --data "{\"transition\":{\"id\":\"761\"}}" -H "Content-Type: application/json" $JIRA_URL/rest/api/2/issue/$jira/transitions?expand=transitions.fields -v
 #changer le status en livraison
